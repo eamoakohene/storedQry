23ace749fb9cd0c7ef2f63641249d1e7a1edcac3
Emmanuel Amoakohene <eamoakohene@gmail.com>
2016-09-09 10:52:55 +0100

    code update
    code update

--- R/examples.R
+++ R/examples.R
@@ -37,3 +37,12 @@
 #
 #     )
 #   )
+# my_list <-  list(
+#     `@i_yr` = 2016,
+#     `@i_mth` = 9,
+#     `@i_dy` = 8,
+#     `@s_data_code` = 'USD',
+#     `@i_data_value` = 1.3359
+# )
+#
+# storedQry::SQ$new( 'R:/shiny/beama/bmonitor/bss.sqlite' )$set_name("tdi_update_indicator")$set_params( my_list )$params_replace()
